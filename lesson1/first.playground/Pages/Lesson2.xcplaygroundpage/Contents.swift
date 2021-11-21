1d8aca669e791e2def45b96b4026117ead0becea
NvIGA <devprogram.w@gmail.com>
2021-11-21 18:32:06 +0200

    lesson 2

---
+++ lesson1/first.playground/Pages/Lesson2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+// optional types
+var wallet:Float?
+
+wallet = 50
+
+if wallet != nil{
+    print(wallet!)
+}
+
+
+if let myWallet = wallet{
+    print(myWallet)
+}else{
+    
+}
+
+
+
+var str = "1dfs"
+
+//print(Int(str)!)
+
+if let intStr = Int(str){
+    print(intStr)
+}else{
+    print("\(str): something wrong")
+}
+
+
+
+let a:Int? = nil
+let b:Int = a ?? 0
+
+print("b equal \(b)")
